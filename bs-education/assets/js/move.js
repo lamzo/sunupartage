7e41f655439677db7b9e24164807618b69ca696f
nabiullahi soumah <coriassphenix@gmail.com>
2017-03-05 15:11:24 -0600

    Add files via upload

--- bs-education/assets/js/move.js
+++ bs-education/assets/js/move.js
@@ -12,7 +12,10 @@ $(document).ready(function()
        $("#first").slideUp("slow",function(){
 	      $("#second").slideDown("fast");
 	   });
-	 });
+	   
+	   
+});
+
 	 
 /******************************************SCROLL DOWN TO THE FORMS *******************************************/
 	 $("#deja").click(function(){
@@ -57,9 +60,137 @@ $(document).ready(function()
                         localStorage.pass = '';
                         localStorage.chkbx = '';
                     }
-                });
+
+
+					});
+		
+/********************************SUBMIT ATTENDRE L'AUTRE SUBMIT*********************************************/
+
+      var submitActor = null;
+      var $form = $('#test');
+      var $submitActors = $form.find('button[type=submit]');
+
+      $form.submit(function(event) {
+          if (null === submitActor) {
+              // If no actor is explicitly clicked, the browser will
+              // automatically choose the first in source-order
+              // so we do the same here
+              submitActor = $submitActors[0];
+          }
+
+          console.log(submitActor.name);
+          // alert(submitActor.name);
+
+          return false;
+      });
+
+      $submitActors.click(function(event) {
+          submitActor = this;
+      });				
+				
+/*****************************************************NEXT ET PRECEDENT DU FORMULAIRE***********************************************/
+
+//jQuery time
+var current_fs, next_fs, previous_fs; //fieldsets
+var left, opacity, scale; //fieldset properties which we will animate
+var animating; //flag to prevent quick multi-click glitches
+
+$(".next").click(function(){
+	if(animating) return false;
+	animating = true;
+	
+	current_fs = $(this).parent();
+	next_fs = $(this).parent().next();
+	
+	//activate next step on progressbar using the index of next_fs
+	$("#progressbar li").eq($(".displaydiv").index(next_fs)).addClass("active");
+	
+	//show the next fieldset
+	next_fs.show(); 
+	//hide the current fieldset with style
+	current_fs.animate({opacity: 0}, {
+		step: function(now, mx) {
+			//as the opacity of current_fs reduces to 0 - stored in "now"
+			//1. scale current_fs down to 80%
+			scale = 1 - (1 - now) * 0.2;
+			//2. bring next_fs from the right(50%)
+			left = (now * 50)+"%";
+			//3. increase opacity of next_fs to 1 as it moves in
+			opacity = 1 - now;
+			current_fs.css({
+        'transform': 'scale('+scale+')',
+        'position': 'relative'
+      });
+			next_fs.css({'left': left, 'opacity': opacity});
+		}, 
+		duration: 800, 
+		complete: function(){
+			current_fs.hide();
+			animating = false;
+		}, 
+		//this comes from the custom easing plugin
+		easing: 'easeInOutBack'
+	});
+});
+
+$(".previous").click(function(){
+	if(animating) return false;
+	animating = true;
+	
+	current_fs = $(this).parent();
+	previous_fs = $(this).parent().prev();
+	
+	//de-activate current step on progressbar
+	$("#progressbar li").eq($(".displaydiv").index(current_fs)).removeClass("active");
+	
+	//show the previous fieldset
+	previous_fs.show(); 
+	//hide the current fieldset with style
+	current_fs.animate({opacity: 0}, {
+		step: function(now, mx) {
+			//as the opacity of current_fs reduces to 0 - stored in "now"
+			//1. scale previous_fs from 80% to 100%
+			scale = 0.8 + (1 - now) * 0.2;
+			//2. take current_fs to the right(50%) - from 0%
+			left = ((1-now) * 50)+"%";
+			//3. increase opacity of previous_fs to 1 as it moves in
+			opacity = 1 - now;
+			current_fs.css({'left': left});
+			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
+		}, 
+		duration: 800, 
+		complete: function(){
+			current_fs.hide();
+			animating = false;
+		}, 
+		//this comes from the custom easing plugin
+		easing: 'easeInOutBack'
+	});
+});
+
+$(".submit").click(function(){
+	return false;
+})
+
+				
 }); 
-/*********************************** NIVEAU DU MOT DE PASSE****************************************/
+
+
+
+/****************************************if select options shoW another options of form*****************************************/
+
+
+    $(".group_tag_dynamic").hide(); //default
+    $("select[name=action_top]").change(function() 
+	{
+        var selectedOption = $(this).find('option:selected');
+        if(selectedOption.hasClass('bulk_action_target'))
+            $(".group_tag_dynamic").show('fast');
+        else if(selectedOption.hasClass('bulk_action_delete'))
+            $(".group_tag_dynamic").hide('fast');
+    });
+
+/***************************************FONCTION AFFICHE STRONG PASSWORD******************************************/
 
 function passwordStrength(password)
 {
@@ -110,12 +241,5 @@ function validatePassword(){
 password.onchange = validatePassword;
 confirm_password.onkeyup = validatePassword;
 
-/*****************************************************HOME PAGE***********************************************/
-function GoToHomePage()
-{
-    /*window.location = '/';   
-	window.location = '/';*/
-
-}
 
 
